1cf61d0e0302f1781022dab1a20c29b4cc9ba19a
hudaokeji <github@hudaokeji.com>
2020-04-25 16:38:27 +0900

    add original version

---
+++ .original/model-viewer-0.10.0/.github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,3 @@
+<!-- Instructions: https://github.com/google/model-viewer/blob/master/packages/model-viewer/CONTRIBUTING.md#code-reviews -->
+### Reference Issue
+<!-- Example: Fixes #1234 -->
---
+++ .original/model-viewer-0.10.0/packages/model-viewer/src/features/staging.ts
@@ -0,0 +1,112 @@
+
+/* @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {property} from 'lit-element';
+
+import ModelViewerElementBase, {$renderer, $scene, $tick} from '../model-viewer-base.js';
+import {Constructor} from '../utilities.js';
+
+import {CameraChangeDetails} from './controls.js';
+
+// How much the model will rotate per
+// second in radians:
+const ROTATION_SPEED = Math.PI / 32;
+export const AUTO_ROTATE_DELAY_DEFAULT = 3000;
+
+const $autoRotateStartTime = Symbol('autoRotateStartTime');
+const $cameraChangeHandler = Symbol('cameraChangeHandler');
+const $onCameraChange = Symbol('onCameraChange');
+
+export declare interface StagingInterface {
+  autoRotate: boolean;
+  autoRotateDelay: number;
+  readonly turntableRotation: number;
+  resetTurntableRotation(): void;
+}
+
+export const StagingMixin = <T extends Constructor<ModelViewerElementBase>>(
+    ModelViewerElement: T): Constructor<StagingInterface>&T => {
+  class StagingModelViewerElement extends ModelViewerElement {
+    @property({type: Boolean, attribute: 'auto-rotate'})
+    autoRotate: boolean = false;
+
+    @property({type: Number, attribute: 'auto-rotate-delay'})
+    autoRotateDelay: number = AUTO_ROTATE_DELAY_DEFAULT;
+
+    private[$autoRotateStartTime] = performance.now();
+    private[$cameraChangeHandler] = (event: CustomEvent<CameraChangeDetails>) =>
+        this[$onCameraChange](event);
+
+    connectedCallback() {
+      super.connectedCallback();
+      this.addEventListener(
+          'camera-change', this[$cameraChangeHandler] as EventListener);
+      this[$autoRotateStartTime] = performance.now();
+    }
+
+    disconnectedCallback() {
+      super.disconnectedCallback();
+      this.removeEventListener(
+          'camera-change', this[$cameraChangeHandler] as EventListener);
+      this[$autoRotateStartTime] = performance.now();
+    }
+
+    updated(changedProperties: Map<string, any>) {
+      super.updated(changedProperties);
+
+      if (changedProperties.has('autoRotate')) {
+        this[$autoRotateStartTime] = performance.now();
+      }
+    }
+
+    [$tick](time: number, delta: number) {
+      super[$tick](time, delta);
+
+      if (!this.autoRotate || !this.modelIsVisible ||
+          this[$renderer].isPresenting) {
+        return;
+      }
+
+      const rotationDelta = Math.min(
+          delta, time - this[$autoRotateStartTime] - this.autoRotateDelay);
+
+      if (rotationDelta > 0) {
+        this[$scene].yaw =
+            this.turntableRotation + ROTATION_SPEED * rotationDelta * 0.001;
+      }
+    }
+
+    [$onCameraChange](event: CustomEvent<CameraChangeDetails>) {
+      if (!this.autoRotate) {
+        return;
+      }
+
+      if (event.detail.source === 'user-interaction') {
+        this[$autoRotateStartTime] = performance.now();
+      }
+    }
+
+    get turntableRotation(): number {
+      return this[$scene].yaw;
+    }
+
+    resetTurntableRotation() {
+      this[$scene].yaw = 0;
+    }
+  }
+
+  return StagingModelViewerElement;
+};
